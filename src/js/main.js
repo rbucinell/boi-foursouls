0ea8afcaaac6578003a0c375d01945084b743c0f
Bucinell, Ryan <rbucinell.dev@gmail.com>
2018-12-28 16:14:36 -0500

    initiated with phaser boilder plate

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 1024,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    
+}
+
+function create ()
+{
+}
+
+function update()
+{
+
+}
